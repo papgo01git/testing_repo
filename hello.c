771c7b86f9bec58ff34cc1483c1f1cabedf94687
papgo01git <gowthamkp.ca@gmail.com>
2015-10-06 15:12:03 +0530

    Jerry: will add new line to test two commits before pushing

--- hello.c
+++ hello.c
@@ -1,3 +1,4 @@
 //test2 by papgo01
 //github changes in Jerry, commit from his area
 //github changes in Jerry, Update after the previous line
+//github changes in Jerry, just commit, will add new line to test two commits before pushing
