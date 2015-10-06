57152de855abe31a5ddbf84e7c4502ba9a822c27
papgo01git <gowthamkp.ca@gmail.com>
2015-10-06 15:19:03 +0530

    Jerry: New change in same commit.

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 //test2 by papgo01
 //github changes in Jerry, commit from his area
 //github changes in Jerry, Update after the previous line
-//github changes in Jerry, just commit, will add new line to test two commits before pushing
+//**github changes in Jerry, just commit, will add new line to test two commits before pushing
+// New change in same commit.
